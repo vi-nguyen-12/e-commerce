bb857bdce6236c9b22a7688dc36c5be7db348c6b
Chloe <chloe.ng.1292@gmail.com>
2021-02-06 13:48:50 +0700

    BE-order

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,44 +1,56 @@
-import asyncHandler from 'express-async-handler';
-import Order from '../models/orderModel.js';
+import asyncHandler from "express-async-handler";
+import Order from "../models/orderModel.js";
 
 //@desc     Create new order
 //@route    POST /api/orders
 //@access   private
-const addOrderItems=asyncHandler(async(req,res)=>{
-    const {orderItems, shippingAddress,paymentMethod,itemsPrice,taxPrice,shippingPrice, totalPrice}=req.body;
+const addOrderItems = asyncHandler(async (req, res) => {
+  const {
+    orderItems,
+    shippingAddress,
+    paymentMethod,
+    itemsPrice,
+    taxPrice,
+    shippingPrice,
+    totalPrice,
+  } = req.body;
 
-    if (orderItems && orderItems.length===0){
-        res.status(404).send();
-        throw new Error('No order items');
-    }else {
-        const order=new Order({
-            orderItems,
-            user:req.user._id, //this is a protected routes
-            shippingAddress,
-            paymentMethod,
-            itemsPrice,
-            taxPrice,
-            shippingPrice,
-            totalPrice
-        })
-        const createdOrder=await order.save() //wait for to be saved; above is just instantiate
+  if (orderItems && orderItems.length === 0) {
+    res.status(404).send();
+    throw new Error("No order items");
+    return;
+  } else {
+    const order = new Order({
+      orderItems,
+      user: req.user._id, //this is a protected routes
+      shippingAddress,
+      paymentMethod,
+      itemsPrice,
+      taxPrice,
+      shippingPrice,
+      totalPrice,
+    });
+    const createdOrder = await order.save(); //wait for to be saved; above is just instantiate
 
-        res.status(201).json(createdOrder);
-    }
-})
+    res.status(201).json(createdOrder);
+  }
+});
 
 //@desc     Get order by ID
 //@route    GET /api/orders/:id
 //@access   private
-const getOrderById=asyncHandler(async(req,res)=>{
-    const order=await Order.findById(req.params.id).populate('user','name email')
+const getOrderById = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id).populate(
+    "user",
+    "name email"
+  );
 
-    if (order){
-        res.json(order)
-    }else{
-        res.status(404);
-        throw new Error('Order not found')
-    }
-})
+  if (order) {
+    res.json(order);
+  } else {
+    res.status(404);
+    throw new Error("Order not found");
+  }
+});
 
-export {addOrderItems, getOrderById}
+export { addOrderItems, getOrderById };
