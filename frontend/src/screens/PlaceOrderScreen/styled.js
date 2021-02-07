80f7e93c9080a4a3ff00d6bdb5c140bcb38404bd
Chloe <chloe.ng.1292@gmail.com>
2021-02-07 10:22:00 +0700

    clean up PlaceOrderScreen

--- frontend/src/screens/PlaceOrderScreen/styled.js
+++ frontend/src/screens/PlaceOrderScreen/styled.js
@@ -1,29 +1,32 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.div`
-.ui.container{
+  .ui.container {
     display: flex;
     justify-content: center;
-    align-items:flex-start;
-}
-.ui.list>.item .header, .ui.header {
+    align-items: flex-start;
+  }
+  .ui.list > .item .header,
+  .ui.header {
     line-height: 1.5rem;
     font-size: 1.1rem;
-    font-weight:350
-}
-.ui.list>.item .header{
-    padding:1rem 0
-}
-.info{
+    font-weight: 350;
+  }
+  .ui.list > .item .header {
+    padding: 1rem 0;
+  }
+  .info {
     width: 800px;
-    padding-right: 20px
-}
-.ui.menu .item {
-        display: flex;
-        justify-content: space-between;
+    padding-right: 20px;
+  }
+  .ui.menu .item {
+    display: flex;
+    justify-content: space-between;
 
-        & .ui.button {
-        width:100%;
-        background-color: black;
-        color:white;
-  `
+    & .ui.button {
+      width: 100%;
+      background-color: black;
+      color: white;
+    }
+  }
+`;
