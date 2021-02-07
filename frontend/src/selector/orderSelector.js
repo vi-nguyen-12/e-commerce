51a3ae81278c359b6a200e350c7cf655522c2899
Chloe <chloe.ng.1292@gmail.com>
2021-02-07 10:20:48 +0700

    orderSlice and orderSelector

--- frontend/src/selector/orderSelector.js
+++ frontend/src/selector/orderSelector.js
@@ -1,5 +1,9 @@
-import {createSelector} from 'reselect';
-export const orderCreateSelector=createSelector(
-    ({orderCreate})=>orderCreate,
-    orderCreate=>orderCreate
-)
+import { createSelector } from "reselect";
+export const orderCreateSelector = createSelector(
+  ({ orderCreate }) => orderCreate,
+  (orderCreate) => orderCreate
+);
+export const orderDetailsSelector = createSelector(
+  ({ orderDetails }) => orderDetails,
+  (orderDetails) => orderDetails
+);
--- frontend/src/slice/orderSlice.js
+++ frontend/src/slice/orderSlice.js
@@ -1,31 +1,65 @@
-import { createSlice,createAsyncThunk} from '@reduxjs/toolkit'
-import {ordersApi} from '../api/ordersApi';
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { ordersApi } from "../api/ordersApi";
 
 // create order
-export const createOrder=createAsyncThunk(
-    'orders/createOrder',
-    async(data)=>{
-        try{
-            const response=await ordersApi.createOrder(data);
-            return {response}
-        }
-        catch(err){
-            const error=err.response&&err.response.data.message
-            ? err.response.data.message
-            :err.message
-            return {error} 
-        }
-})
-export const orderCreateSlice=createSlice({
-    name:'orderCreate',
-    initialState:{order:{}},
-    extraReducers:{
-        [createOrder.fulfilled]:(state,{payload})=>{
-            if("response" in payload){
-                state.order=payload.response
-                state.success=true
-             }
-            else{state.error=payload.error}       
-        },
+export const createOrder = createAsyncThunk(
+  "orders/createOrder",
+  async (data) => {
+    try {
+      const response = await ordersApi.createOrder(data);
+      return { response };
+    } catch (err) {
+      const error =
+        err.response && err.response.data.message
+          ? err.response.data.message
+          : err.message;
+      return { error };
     }
-})
+  }
+);
+export const orderCreateSlice = createSlice({
+  name: "orderCreate",
+  initialState: { order: {} },
+  extraReducers: {
+    [createOrder.fulfilled]: (state, { payload }) => {
+      if ("response" in payload) {
+        state.order = payload.response;
+        state.success = true;
+      } else {
+        state.error = payload.error;
+      }
+    },
+  },
+});
+
+//get order details
+export const getOrderDetails = createAsyncThunk(
+  "orders/getOrderDetails",
+  async (id) => {
+    try {
+      const response = await ordersApi.getOrderDetail(id);
+      return { response };
+    } catch (err) {
+      const error =
+        err.response && err.response.data.message
+          ? err.response.data.message
+          : err.message;
+      return { error };
+    }
+  }
+);
+export const orderDetailsSlice = createSlice({
+  name: "orderDetails",
+  initialState: { loading: true, order: {} },
+  extraReducers: {
+    [getOrderDetails.fulfilled]: (state, { payload }) => {
+      if ("response" in payload) {
+        state.order = payload.response;
+        state.loading = false;
+      } else {
+        state.error = payload.error;
+        state.loading = false;
+      }
+    },
+  },
+});
