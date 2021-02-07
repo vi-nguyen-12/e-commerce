31e01eedae85233c889d9968aada11a0eef3485d
Chloe <chloe.ng.1292@gmail.com>
2021-02-07 10:20:18 +0700

    create axios to get order Api

--- frontend/src/api/ordersApi.js
+++ frontend/src/api/ordersApi.js
@@ -2,6 +2,9 @@ import axiosClient from "./axiosClient";
 
 export const ordersApi = {
   createOrder: (order) => {
-    return axiosClient.post("/api/orders",order);
+    return axiosClient.post("/api/orders", order);
   },
-}
+  getOrderDetail: (id) => {
+    return axiosClient.get(`/api/orders/${id}`);
+  },
+};
