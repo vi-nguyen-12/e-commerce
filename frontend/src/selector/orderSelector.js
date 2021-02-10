b15f628d1e46a147ff94bcd313394609a4e8bfb2
Chloe <chloe.ng.1292@gmail.com>
2021-02-10 16:16:41 +0700

    fix bugs when log out- reset userDetails and log

--- frontend/src/selector/orderSelector.js
+++ frontend/src/selector/orderSelector.js
@@ -7,3 +7,11 @@ export const orderDetailsSelector = createSelector(
   ({ orderDetails }) => orderDetails,
   (orderDetails) => orderDetails
 );
+export const orderPaySelector = createSelector(
+  ({ orderPay }) => orderPay,
+  (orderPay) => orderPay
+);
+export const myOrderListSelector = createSelector(
+  ({ myOrderList }) => myOrderList,
+  (myOrderList) => myOrderList
+);
