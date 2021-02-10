e95c40bee3c247c52cfc7e1ca42aecbcb9abc0dc
Chloe <chloe.ng.1292@gmail.com>
2021-02-10 22:45:02 +0700

    FE- api to get userList

--- frontend/src/api/usersApi.js
+++ frontend/src/api/usersApi.js
@@ -1,15 +1,18 @@
 import axiosClient from "./axiosClient";
 export const usersApi = {
   login: (data) => {
-    return axiosClient.post("/api/users/login",data);
+    return axiosClient.post("/api/users/login", data);
   },
-  register:(data)=>{
-    return axiosClient.post("/api/users",data);
+  register: (data) => {
+    return axiosClient.post("/api/users", data);
   },
-  getProfile:(id)=>{
+  getProfile: (id) => {
     return axiosClient.get(`/api/users/${id}`);
   },
-  updateProfile:(user)=>{
-    return axiosClient.put('api/users/profile',user)
-  }
+  updateProfile: (user) => {
+    return axiosClient.put("api/users/profile", user);
+  },
+  listUsers: () => {
+    return axiosClient.get("/api/users");
+  },
 };
