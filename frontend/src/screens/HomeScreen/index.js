a01feb3d2b1c7eb9cb3ed3e7accdae76e5ce7fa0
Chloe <chloe.ng.1292@gmail.com>
2021-02-11 08:44:44 +0700

    FE- functionality to delete user

--- frontend/src/screens/HomeScreen/index.js
+++ frontend/src/screens/HomeScreen/index.js
@@ -1,35 +1,33 @@
-import React, {useEffect} from 'react';
-import {useSelector, useDispatch} from 'react-redux';
-import {Header, Grid, Message} from 'semantic-ui-react'
-import { Wrapper} from "./styled";
-import {Product} from '../../components'
-import {productListSelector} from '../../selector/productSelector';
-import {getProductList} from '../../slice/productSlice'
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Header, Grid, Message } from "semantic-ui-react";
+import { Wrapper } from "./styled";
+import { Product } from "../../components";
+import { productListSelector } from "../../selector/productSelector";
+import { getProductList } from "../../slice/productSlice";
 
 const HomeScreen = () => {
-    const {products,error}=useSelector(productListSelector);
-    console.log(products);
-    const dispatch=useDispatch();
-    useEffect(()=>{
-       dispatch(getProductList());
-    },[dispatch])
+  const { products, error } = useSelector(productListSelector);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getProductList());
+  }, [dispatch]);
 
+  if (error) return <Message error header={error} />;
+  return (
+    <Wrapper>
+      <Header> LATEST PRODUCTS </Header>
+      <Grid columns={4}>
+        <Grid.Row>
+          {products.map((product, idx) => (
+            <Grid.Column key={idx}>
+              <Product product={product} />
+            </Grid.Column>
+          ))}
+        </Grid.Row>
+      </Grid>
+    </Wrapper>
+  );
+};
 
-    if(error) return <Message error header={error}/>
-    return (
-            <Wrapper>
-                <Header> LATEST PRODUCTS </Header>
-                <Grid columns={4}>
-                    <Grid.Row>
-                        {products.map((product,idx)=>(
-                            <Grid.Column key={idx}>
-                                <Product product={product}/>
-                            </Grid.Column>
-                        ))}
-                    </Grid.Row>
-                </Grid>
-            </Wrapper>
-    )
-}
-
-export default HomeScreen
+export default HomeScreen;
