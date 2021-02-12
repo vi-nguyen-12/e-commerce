59a2a94dfe7ba0f123e5845285bc577be8a2df6c
Chloe <chloe.ng.1292@gmail.com>
2021-02-12 16:59:17 +0700

    FE- slice to delte product

--- frontend/src/slice/productSlice.js
+++ frontend/src/slice/productSlice.js
@@ -1,36 +1,66 @@
-import { createSlice,createAsyncThunk } from '@reduxjs/toolkit'
-import {productsApi} from '../api/productsApi'
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { productsApi } from "../api/productsApi";
 
 //productList
-export const getProductList=createAsyncThunk('products/getProductList',async()=>{
-        const res=await productsApi.getProducts();
-        return res
-})
-export const productListSlice=createSlice({
-    name:'productList',
-    initialState:{products:[]},
-    extraReducers:{
-        [getProductList.fulfilled]:(state,{payload})=>{
-            state.products=payload;
-        }
-    }
-})
+export const getProductList = createAsyncThunk(
+  "products/getProductList",
+  async () => {
+    const res = await productsApi.getProducts();
+    return res;
+  }
+);
+export const productListSlice = createSlice({
+  name: "productList",
+  initialState: { products: [] },
+  extraReducers: {
+    [getProductList.fulfilled]: (state, { payload }) => {
+      state.products = payload;
+    },
+  },
+});
 
 //productDetail
-export const getProductDetail=createAsyncThunk('products/getProductDetail',async(id)=>{
-    const res=await productsApi.getProduct(id);
-    return res
+export const getProductDetail = createAsyncThunk(
+  "products/getProductDetail",
+  async (id) => {
+    const res = await productsApi.getProduct(id);
+    return res;
+  }
+);
+export const productDetailSlice = createSlice({
+  name: "productDetail",
+  initialState: { product: {} },
+  extraReducers: {
+    [getProductDetail.fulfilled]: (state, { payload }) => {
+      state.product = payload;
+    },
+    [getProductDetail.rejected]: (state, { payload }) => {
+      state.error = payload;
+    },
+  },
+});
+// delete product
+export const deleteProduct = createAsyncThunk(
+  "products/deleteProduct",
+  async (id) => {
+    try {
+      const response = await productsApi.deleteProduct(id);
+      return { response };
+    } catch (error) {
+      return { error };
     }
-)
-export const productDetailSlice=createSlice({
-    name:'productDetail',
-    initialState:{product:{}},
-    extraReducers:{
-        [getProductDetail.fulfilled]:(state,{payload})=>{
-            state.product=payload
-        },
-        [getProductDetail.rejected]:(state,{payload})=>{
-            state.error=payload
-        },
-    }
-})
+  }
+);
+export const productDeleteSlice = createSlice({
+  name: "productDelete",
+  initialState: {},
+  extraReducers: {
+    [deleteProduct.fulfilled]: (state, { payload }) => {
+      if ("response" in payload) {
+        state.success = true;
+      } else {
+        state.error = payload.error;
+      }
+    },
+  },
+});
