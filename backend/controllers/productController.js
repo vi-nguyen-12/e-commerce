5ad214bfe9ff4e2423db0c1339fed434dfec2a95
Chloe <chloe.ng.1292@gmail.com>
2021-02-12 16:54:59 +0700

    BE- routes and controllers- delete product

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,26 +1,41 @@
-import asyncHandler from 'express-async-handler';
-import Product from '../models/productModel.js';
+import asyncHandler from "express-async-handler";
+import Product from "../models/productModel.js";
 
 //@desc     Fetch all products
 //@route    GET /api/products
 //@access   public
-const getProducts=asyncHandler(async(req,res)=>{
-    const products=await Product.find({})
-    res.json(products)
-})
+const getProducts = asyncHandler(async (req, res) => {
+  const products = await Product.find({});
+  res.json(products);
+});
 
 //@desc     Fetch single product
 //@route    GET /api/products/:id
 //@access   public
-const getProductById=asyncHandler(async(req,res)=>{
-    const product=await Product.findById(req.params.id)
+const getProductById = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id);
 
-    if(product){
-        res.json(product)
-    }
-    else{
-        res.status(404)
-        throw new Error('Product not found')
-}})
+  if (product) {
+    res.json(product);
+  } else {
+    res.status(404);
+    throw new Error("Product not found");
+  }
+});
 
-export {getProducts, getProductById}
+//@desc     Delete a product
+//@route    DELETE /api/products/:id
+//@access   private/ admin
+const deleteProduct = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    await product.remove();
+    res.json({ message: "Product removed" });
+  } else {
+    res.status(404);
+    throw new Error("Product not found");
+  }
+});
+
+export { getProducts, getProductById, deleteProduct };
