e6219ace429643bb341fc484b35b2c0963fe97b8
Chloe <chloe.ng.1292@gmail.com>
2021-02-12 17:00:45 +0700

    FE- delete product selector

--- frontend/src/selector/productSelector.js
+++ frontend/src/selector/productSelector.js
@@ -1,11 +1,16 @@
-import {createSelector} from 'reselect';
+import { createSelector } from "reselect";
 
-export const productListSelector=createSelector(
-    ({productList})=>productList,
-    productList=>productList
-)
+export const productListSelector = createSelector(
+  ({ productList }) => productList,
+  (productList) => productList
+);
 
-export const productDetailSelector=createSelector(
-    ({productDetail})=>productDetail,
-    productDetail=>productDetail
-)
+export const productDetailSelector = createSelector(
+  ({ productDetail }) => productDetail,
+  (productDetail) => productDetail
+);
+
+export const productDeleteSelector = createSelector(
+  ({ productDelete }) => productDelete,
+  (productDelete) => productDelete
+);
