37467a3698a9b2bde44744986c4dd9430d13cd0a
Chloe <chloe.ng.1292@gmail.com>
2021-02-18 06:22:42 +0700

    FE- product edit slice

--- frontend/src/slice/productSlice.js
+++ frontend/src/slice/productSlice.js
@@ -1,6 +1,95 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import { productsApi } from "../api/productsApi";
 
+//update product
+export const updateProduct = createAsyncThunk(
+  "products/updateProduct",
+  async (product) => {
+    try {
+      const response = await productsApi.updateProduct(product);
+      return { response };
+    } catch (error) {
+      return { error };
+    }
+  }
+);
+export const productUpdateSlice = createSlice({
+  name: "productUpdate",
+  initialState: {},
+  reducers: {
+    resetUpdate: (state) => (state = {}),
+  },
+  extraReducers: {
+    [updateProduct.fulfilled]: (state, { payload }) => {
+      if ("response" in payload) {
+        state.success = true;
+        state.product = payload.response;
+      } else {
+        state.error = payload.error;
+      }
+    },
+  },
+});
+export const { resetUpdate } = productUpdateSlice.actions;
+
+// create product
+export const createProduct = createAsyncThunk(
+  "products/createProduct",
+  async () => {
+    try {
+      const response = await productsApi.createProduct();
+      return { response };
+    } catch (error) {
+      return { error };
+    }
+  }
+);
+
+export const productCreateSlice = createSlice({
+  name: "productCreate",
+  initialState: {},
+  reducers: {
+    resetProduct: () => {},
+  },
+  extraReducers: {
+    [createProduct.fulfilled]: (state, { payload }) => {
+      if ("response" in payload) {
+        state.success = true;
+        state.product = payload.response;
+      } else {
+        state.error = payload.error;
+      }
+    },
+  },
+});
+export const { resetProduct } = productCreateSlice.actions;
+
+// delete product
+export const deleteProduct = createAsyncThunk(
+  "products/deleteProduct",
+  async (id) => {
+    try {
+      const response = await productsApi.deleteProduct(id);
+      return { response };
+    } catch (error) {
+      return { error };
+    }
+  }
+);
+export const productDeleteSlice = createSlice({
+  name: "productDelete",
+  initialState: {},
+  extraReducers: {
+    [deleteProduct.fulfilled]: (state, { payload }) => {
+      if ("response" in payload) {
+        state.success = true;
+      } else {
+        state.error = payload.error;
+      }
+    },
+  },
+});
+
 //productList
 export const getProductList = createAsyncThunk(
   "products/getProductList",
@@ -37,65 +126,8 @@ export const productDetailSlice = createSlice({
     [getProductDetail.rejected]: (state, { payload }) => {
       state.error = payload;
     },
-  },
-});
-// delete product
-export const deleteProduct = createAsyncThunk(
-  "products/deleteProduct",
-  async (id) => {
-    try {
-      const response = await productsApi.deleteProduct(id);
-      return { response };
-    } catch (error) {
-      return { error };
-    }
-  }
-);
-export const productDeleteSlice = createSlice({
-  name: "productDelete",
-  initialState: {},
-  extraReducers: {
-    [deleteProduct.fulfilled]: (state, { payload }) => {
-      if ("response" in payload) {
-        state.success = true;
-      } else {
-        state.error = payload.error;
-      }
+    [resetUpdate]: (state) => {
+      state.product = {};
     },
   },
 });
-
-// create product
-export const createProduct = createAsyncThunk(
-  "products/createProduct",
-  async () => {
-    try {
-      const response = await productsApi.createProduct();
-      return { response };
-    } catch (error) {
-      return { error };
-    }
-  }
-);
-
-export const productCreateSlice = createSlice({
-  name: "productCreate",
-  initialState: {},
-  reducers: {
-    resetProduct: (state) => {
-      state = {};
-    },
-  },
-  extraReducers: {
-    [createProduct.fulfilled]: (state, { payload }) => {
-      if ("response" in payload) {
-        state.success = true;
-        state.product = payload.response;
-      } else {
-        state.error = payload.error;
-      }
-    },
-  },
-});
-
-export const { resetProduct } = productCreateSlice.actions;
