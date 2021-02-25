6e7000efc189af333ca17ceb4652754409799931
Chloe <chloe.ng.1292@gmail.com>
2021-02-26 05:03:51 +0700

    FE- Search component

--- frontend/src/components/Search/index.js
+++ frontend/src/components/Search/index.js
@@ -1,10 +1,17 @@
 import React, { useState } from "react";
+import { useDispatch } from "react-redux";
 import { Form, Button } from "semantic-ui-react";
 import { Wrapper } from "./styled";
+import { getProductList } from "../../slice/productSlice";
+import { get } from "mongoose";
 
 const Search = () => {
   const [keyword, setKeyword] = useState("");
-  const handleSubmit = () => {};
+  const dispatch = useDispatch();
+  const handleSubmit = () => {
+    dispatch(getProductList(keyword));
+    setKeyword = "";
+  };
   return (
     <Wrapper>
       <Form size="tiny">
