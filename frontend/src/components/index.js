94499f64d7a675322ad70d3d5b7c2ebce953fc42
Chloe <chloe.ng.1292@gmail.com>
2021-02-28 16:12:19 +0700

    FE- Paginate component

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -1,6 +1,7 @@
-import Heading from './Heading';
-import Product from './Product';
-import Loading from './Loading';
-import CheckoutSteps from './CheckoutSteps';
+import Heading from "./Heading";
+import Product from "./Product";
+import Loading from "./Loading";
+import CheckoutSteps from "./CheckoutSteps";
+import Paginate from "./Paginate";
 
-export {Heading, Product,Loading,CheckoutSteps};
+export { Heading, Product, Loading, CheckoutSteps, Paginate };
