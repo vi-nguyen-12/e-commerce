d19253fd80ba1cc441909cf23fd83e4380207163
Chloe <chloe.ng.1292@gmail.com>
2021-03-08 17:34:40 +0700

    FE- ProductCarousel component

---
+++ frontend/src/components/ProductCarousel/styled.js
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  /* .carousel-root {
+    display: flex;
+    flex-direction: row;
+  } */
+  img {
+    object-fit: contain;
+    width: 40% !important;
+    border-radius: 50%;
+  }
+  h4 {
+    padding-top: 10px;
+    background-color: white;
+  }
+  .thumbs-wrapper {
+    display: none;
+  }
+`;
