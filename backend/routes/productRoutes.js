9628a6cc6318a6598e49203b847c6ec37a01d0b5
Chloe <chloe.ng.1292@gmail.com>
2021-03-08 17:17:48 +0700

    BE- getTopProducts routes

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -12,11 +12,11 @@ import {
 import { protect, admin } from "../middleware/authMiddleware.js";
 
 router.route("/").get(getProducts).post(protect, admin, createProduct);
+router.get("/top", getTopProducts);
 router
   .route("/:id")
   .get(getProductById)
   .delete(protect, admin, deleteProduct)
   .put(protect, admin, updateProduct);
 router.route("/:id/reviews").post(protect, createProductReview);
-router.get("/top", getTopProducts);
 export default router;
